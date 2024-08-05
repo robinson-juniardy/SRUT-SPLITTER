8f89b1e241f5c102e7c0ae078bf30758e5ba52cd
robinson-juniardy <robinson.juniardy@gmail.com>
2024-08-06 00:58:38 +0800

    first commit

---
+++ src/main/java/com/triomotor/triosrut/TrioSrut.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.triomotor.triosrut;
+
+/**
+ *
+ * @author robin
+ */
+public class TrioSrut {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+}
